981d48e02555d5aea94adbaff8863b698eb87cf8
grbalmeida <g.r.almeida@live.com>
2025-10-05 19:29:01 -0300

    exercicios 2.1 e 2.2

---
+++ capitulo2/exercicio2.1.py
@@ -0,0 +1,9 @@
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+
+# 10 + 20 X 30
+# 4 elevado a 2 ÷ 30
+# (9 elevado a 4 + 2) X 6 - 1
+
+print(10 + 20 * 30) # 610
+print(4**2 / 30) # 0.533...
+print(9**4 + 2) * 6 - 1 # 39377
