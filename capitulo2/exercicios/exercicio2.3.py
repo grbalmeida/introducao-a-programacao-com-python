f0fe739b17b9df6da5042d60f42a12a2be27cfac
grbalmeida <g.r.almeida@live.com>
2025-10-05 22:40:12 -0300

    exercicios 2.3, 2.4, 2.5, 2.6 e programas 2.1, 2.2 e 2.3

---
+++ capitulo2/exercicios/exercicio2.3.py
@@ -0,0 +1,2 @@
+# Exercício 2.3: Faça um programa que exiba seu nome na tela
+print("Gilvan")
---
+++ capitulo2/exercicios/exercicio2.6.py
@@ -0,0 +1,6 @@
+# Exercício 2.6: Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750
+
+salário = 750
+aumento = 15
+print(salário + (salário * aumento / 100)) # 862.5 # novo salário
+print(salário * aumento / 100) # 112.5 valor aumentado
