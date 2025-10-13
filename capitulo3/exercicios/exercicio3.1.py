2f76d9f8438a9927e5cdd5df8e016da44bc9ae0c
grbalmeida <g.r.almeida@live.com>
2025-10-13 20:40:58 -0300

    exercício 3.1

---
+++ capitulo3/exercicios/exercicio3.1.py
@@ -0,0 +1,16 @@
+# Exercício 3.1: Complete a tabela a seguir, marcando inteiro ou ponto flutuante dependendo do número apresentado.
+
+# Número | Tipo numérico
+# 5 inteiro ponto flutuante
+# 5.0 inteiro ponto flutuante
+# 4.3 inteiro ponto flutuante
+# -2 inteiro ponto flutuante
+# 100 inteiro ponto flutuante
+# 1.333 inteiro ponto flutuante
+
+print(type(5)) # int
+print(type(5.0)) # float
+print(type(4.3)) # float
+print(type(-2)) # int
+print(type(100)) # int
+print(type(1.333)) # float
