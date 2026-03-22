5bf8f7155a42d7238a3c06f9a2523b53810c7b53
grbalmeida <g.r.almeida@live.com>
2026-03-22 17:28:14 -0300

    Programas 6.1, 6.2, 6.3 e exercício 6.1

---
+++ capitulo6/exercicios/exercicio6.1.py
@@ -0,0 +1,17 @@
+# Exercício 6.1: Modifique o Programa 6.2 para ler 7 notas em vez de 5.
+notas = [0, 0, 0, 0, 0, 0, 0]
+soma = 0
+x = 0
+
+while x < 7:
+    notas[x] = float(input(f"Nota {x}: "))
+    soma += notas[x]
+    x += 1
+
+x = 0
+
+while x < 7:
+    print(f"Nota: {x}: {notas[x]:6.2f}")
+    x += 1
+
+print(f"Média: {soma / x:5.2f}")
